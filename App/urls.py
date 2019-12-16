ec69379c230a47b5f46061bf793c68ae39865af2
s3arajgupta <swaraj.gupta217@gmail.com>
2019-12-17 01:14:45 +0530

    Signup and login

--- App/urls.py
+++ App/urls.py
@@ -4,10 +4,10 @@ from django.urls import path, include
 from blogs import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home"),
-
-
+    path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
