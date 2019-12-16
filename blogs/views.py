cc8139fa25fe481028cdcf985b48dbb62ced9ea5
s3arajgupta <swaraj.gupta217@gmail.com>
2019-12-17 02:40:29 +0530

    note added to admin

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Blog
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
     return render(request, 'blogs/home.html')
+
+@login_required
+def create(request):
+    if request.method == "POST":
+        if request.POST['title'] and request.POST['body']:
+            blog = Blog()
+            blog.title = request.POST['title']
+            blog.body = request.POST['body']
+            blog.url = request.POST['url']
+            blog.image = request.FILES['image']
+            blog.pub_date = timezone.datetime.now()
+            blog.save()
+            return redirect('home')
+
+        else :
+            return render(request, 'blogs/create.html',{'error':'Title and Body are required to create.'})
+    return render(request, 'blogs/create.html')
